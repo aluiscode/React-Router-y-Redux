eb073dc1c5aa1ac7b317fb4e5caaec69354fba75
Luis LC <luis.lazcanocruz@gmail.com>
2020-07-05 13:56:22 -0500

    Clase 13 -Creando los reducers

--- src/components/CarruselItem.jsx
+++ src/components/CarruselItem.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { setFavorite } from '../actions';
 import '../assets/styles/components/CarruselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
 
-const CarruselItem = ({ cover, title, year, contentRating, duration }) => {
+const CarruselItem = (props) => {
+  const { cover, title, year, contentRating, duration } = props;
+
+  const handleSetFavorite = () => {
+    props.setFavorite({
+      cover, title, year, contentRating, duration,
+    });
+  };
+
   return (
     <div className='carousel-item'>
       <img className='carousel-item__img' src={cover} alt={title} />
       <div className='carousel-item__details'>
         <div>
           <img className='carousel-item__details--img' src={playIcon} alt='Play Icon' />
-          <img className='carousel-item__details--img' src={plusIcon} alt='Plus Icon' />
+          <img className='carousel-item__details--img' src={plusIcon} alt='Plus Icon' onClick={handleSetFavorite} />
         </div>
         <p className='carousel-item__details--title'>{title}</p>
         <p className='carousel-item__details--subtitle'>{`${year} ${contentRating} ${duration} minutos`}</p>
@@ -28,4 +38,8 @@ CarruselItem.propTypes = {
   duration: PropTypes.number,
 };
 
-export default CarruselItem;
+const mapDispatchToProps = {
+  setFavorite,
+};
+
+export default connect(null, mapDispatchToProps)(CarruselItem);
